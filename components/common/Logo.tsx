c24be5d43690a9a9e6c19e2b1c8b4063ecb40a24
Anibal Garcia <moigarcia2000@gmail.com>
2022-03-27 18:02:00 -0500

    Adding Logo

---
+++ components/common/Logo.tsx
@@ -0,0 +1,10 @@
+
+const Logo = () => {
+  return (
+    <div>
+      <img src="logo.png" alt="Logo" />
+    </div>
+  );
+};
+
+export default Logo;
---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from "../common/Logo";
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Logo></Logo>
+    </nav>
+  );
+};
+
+export default Navbar;
